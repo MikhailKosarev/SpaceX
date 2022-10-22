6ce1a1da207a6453d53b2f1b50640afbc5424e85
Mikhail Kosarev <michael.kosarev@gmail.com>
2022-10-22 21:34:13 +0200

    create initial project and delete storyboard

---
+++ SpaceX/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  SpaceX
+//
+//  Created by Mikhail on 22.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
