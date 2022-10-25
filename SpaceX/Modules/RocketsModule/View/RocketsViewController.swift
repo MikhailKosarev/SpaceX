5dbce8aa34fdac69f3af83229c95531337682871
Mikhail Kosarev <michael.kosarev@gmail.com>
2022-10-25 22:35:11 +0200

    add rocketImageView

--- SpaceX/Modules/RocketsModule/View/RocketsViewController.swift
+++ SpaceX/Modules/RocketsModule/View/RocketsViewController.swift
@@ -6,13 +6,47 @@
 //
 
 import UIKit
+import SnapKit
 
 class RocketsViewController: UIViewController {
-
+    
+    // MARK: - Declaring UI elesetupViewments
+    private let rocketImageView: UIImageView = {
+        let image = UIImage(named: "mockRocket")
+        let imageView = UIImageView(image: image)
+        imageView.contentMode = .scaleAspectFit
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    
+    // MARK: - Life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupView()
+        setConstraints()
     }
-
-
+    
+    // MARK: - Private methods
+    private func setupView() {
+        view.addSubview(rocketImageView)
+    }
+    
+    private func setConstraints() {
+        rocketImageView.snp.makeConstraints { make in
+            make.leading.top.trailing.equalTo(view.safeAreaLayoutGuide)
+        }
+    }
 }
 
+
+// MARK: - Preview
+#if DEBUG
+import SwiftUI
+
+struct RocketsViewController_Preview: PreviewProvider {
+    static var previews: some View {
+        RocketsViewController().showPreview()
+    }
+}
+#endif
