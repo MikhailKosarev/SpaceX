301dc2f473fbd9ac7361df35b933f74f07bd3ff8
Mikhail Kosarev <michael.kosarev@gmail.com>
2022-10-27 14:01:37 +0200

    add special colors to the UIColor extension

---
+++ SpaceX/Extensions/UIColor + Extension.swift
@@ -0,0 +1,21 @@
+//
+//  UIColor + Extension.swift
+//  SpaceX
+//
+//  Created by Mikhail on 27.10.2022.
+//
+
+import UIKit
+
+extension UIColor {
+    // text
+    static let specialTextF6F6F6 = #colorLiteral(red: 0.9647058824, green: 0.9647058824, blue: 0.9647058824, alpha: 1)  // F6F6F6
+    static let specialTextD4D4D4 = #colorLiteral(red: 0.831372549, green: 0.831372549, blue: 0.831372549, alpha: 1)  // D4D4D4
+    static let specialTextCACACA = #colorLiteral(red: 0.7921568627, green: 0.7921568627, blue: 0.7921568627, alpha: 1)  // CACACA
+    static let specialText9F9FA0 = #colorLiteral(red: 0.6235294118, green: 0.6235294118, blue: 0.6274509804, alpha: 1)  // 9F9FA0
+    static let specialText8E8E8F = #colorLiteral(red: 0.5568627451, green: 0.5568627451, blue: 0.5607843137, alpha: 1)  // 8E8E8F
+    static let specialText121212 = #colorLiteral(red: 0.07058823529, green: 0.07058823529, blue: 0.07058823529, alpha: 1)  // 121212
+    
+    // background
+    static let specialBackground212121 = #colorLiteral(red: 0.1294117647, green: 0.1294117647, blue: 0.1294117647, alpha: 1)  // 212121
+}
