96a6c25310fe7214eb33b849868fb388b10dbde8
Mikhail Kosarev <michael.kosarev@gmail.com>
2022-10-27 14:25:43 +0200

    add UIFont + Extension with Raleway font

---
+++ SpaceX/Extensions/UIFont + Extension.swift
@@ -0,0 +1,38 @@
+//
+//  UIFont + Extension.swift
+//  SpaceX
+//
+//  Created by Mikhail on 27.10.2022.
+//
+
+import UIKit
+
+extension UIFont {
+    
+    // Regular
+    static func ralewayRegular14() -> UIFont? {
+        return UIFont(name: "Raleway-Regular", size: 14)
+    }
+    
+    static func ralewayRegular16() -> UIFont? {
+        return UIFont(name: "Raleway-Regular", size: 16)
+    }
+    
+    static func ralewayRegular20() -> UIFont? {
+        return UIFont(name: "Raleway-Regular", size: 20)
+    }
+    
+    static func ralewayRegular24() -> UIFont? {
+        return UIFont(name: "Raleway-Regular", size: 24)
+    }
+    
+    
+    // SemiBold
+    static func ralewaySemiBold14() -> UIFont? {
+        return UIFont(name: "Raleway-SemiBold", size: 14)
+    }
+    
+    static func ralewaySemiBold16() -> UIFont? {
+        return UIFont(name: "Raleway-SemiBold", size: 16)
+    }
+}
